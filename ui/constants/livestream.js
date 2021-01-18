6e30af3d8d607624e9190af14fdc3e260e7d8499
Sean Yesmunt <sean@lbry.io>
2021-01-18 14:43:59 -0500

    jiggy

--- ui/constants/livestream.js
+++ ui/constants/livestream.js
@@ -1,6 +1,7 @@
-export const LIVE_STREAM_CHANNEL = '@grin';
-export const LIVE_STREAM_CHANNEL_CLAIM_ID = '4510d86d45c69e6e176394ba2180cf2054fe66e4';
+export const LIVE_STREAM_CHANNEL = '@thomas.zarebczan';
+export const LIVE_STREAM_CHANNEL_CLAIM_ID = 'cb7ea953be131d156a780ff400414a187123e589';
 export const BITWAVE_API = 'https://api.bitwave.tv/v1/channels';
 export const BITWAVE_EMBED_URL = 'https://bitwave.tv/embed';
 // export const BITWAVE_USERNAME = 'lbry';
-export const BITWAVE_USERNAME = 'doomtube';
+// export const BITWAVE_USERNAME = 'doomtube';
+export const BITWAVE_USERNAME = 'thomas.zarebczan';
