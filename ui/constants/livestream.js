76529dda67b3a32c70ab96dae03c09c14901f711
Sean Yesmunt <sean@lbry.io>
2021-01-18 14:29:16 -0500

    update

--- ui/constants/livestream.js
+++ ui/constants/livestream.js
@@ -2,5 +2,5 @@ export const LIVE_STREAM_CHANNEL = '@grin';
 export const LIVE_STREAM_CHANNEL_CLAIM_ID = '4510d86d45c69e6e176394ba2180cf2054fe66e4';
 export const BITWAVE_API = 'https://api.bitwave.tv/v1/channels';
 export const BITWAVE_EMBED_URL = 'https://bitwave.tv/embed';
-export const BITWAVE_USERNAME = 'lbry';
-// export const BITWAVE_USERNAME = 'doomtube';
+// export const BITWAVE_USERNAME = 'lbry';
+export const BITWAVE_USERNAME = 'doomtube';
